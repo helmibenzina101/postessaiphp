13b06cb55e90f1977a1c91a945c6f12e4c9b2478
helmibenzina101 <helmi.benzina@gmail.com>
2023-02-03 13:03:02 +0100

    add values to sql through php

--- index.php
+++ index.php
@@ -2,13 +2,23 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname = "shop";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
-
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+
+$sql = "INSERT INTO produit (id, name, price, qte) VALUES (NULL, 'pizza', 1.11, 1)";
+
+
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 ?>
