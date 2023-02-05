b6b2322cbfb1ac17089ad6d64563a225a773d443
helmibenzina101 <helmi.benzina@gmail.com>
2023-02-05 21:03:34 +0100

    final project in home

---
+++ add.php
@@ -0,0 +1,36 @@
+
+
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "shop";
+$pname=$_POST["name"];
+$pprice=$_POST["price"];
+$pqte=$_POST["qte"];
+
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO produit (id, name, price, qte) VALUES (NULL,'$pname','$pprice', '$pqte')";
+
+
+
+if (mysqli_query($conn, $sql)) 
+{
+  echo "new record created successfully";
+} 
+else 
+{
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+?>
