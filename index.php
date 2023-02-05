894977ef0c235283432f153af5390df9a62a5ef8
helmibenzina101 <helmi.benzina@gmail.com>
2023-02-05 21:12:17 +0100

    adding delete buttton

--- index.php
+++ index.php
@@ -23,6 +23,20 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
+// Check if the delete button is clicked
+if (isset($_GET['delete'])) {
+  $id = $_GET['delete'];
+
+  // Delete the record with the specified ID
+  $sql = "DELETE FROM produit WHERE id=$id";
+
+  if (mysqli_query($conn, $sql)) {
+    echo "Record deleted successfully";
+  } else {
+    echo "Error deleting record: " . mysqli_error($conn);
+  }
+}
+
 $sql = "SELECT id, name, price, qte FROM produit";
 $result = mysqli_query($conn, $sql);
 
@@ -33,6 +47,7 @@ if (mysqli_num_rows($result) > 0) {
   echo "<th>Name</th>";
   echo "<th>Price</th>";
   echo "<th>Quantity</th>";
+  echo "<th>Action</th>";
   echo "</tr>";
   // Output data of each row
   while ($row = mysqli_fetch_assoc($result)) {
@@ -41,6 +56,7 @@ if (mysqli_num_rows($result) > 0) {
     echo "<td>" . $row["name"] . "</td>";
     echo "<td>" . $row["price"] . "</td>";
     echo "<td>" . $row["qte"] . "</td>";
+    echo "<td><a href='?delete=" . $row["id"] . "'>Delete</a></td>";
     echo "</tr>";
   }
   echo "</table>";
